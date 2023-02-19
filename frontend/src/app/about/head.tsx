8058f9d24d74cdfa9dcfe203ef9fb5d6254894e6
Kritik Sah <kritik.sah@truemeds.in>
2023-02-19 20:48:44 +0530

    next 13 fixed v1

---
+++ frontend/src/app/about/head.tsx
@@ -0,0 +1,13 @@
+export default function Head() {
+  return (
+    <>
+      <title>Fitndur | About us</title>
+      <meta
+        name="description"
+        content="Wherever you go, use Fitndur to locate nearby gyms,fitness clubs and pay only for the days you really use them."
+      />
+      <meta content="width=device-width, initial-scale=1" name="viewport" />
+      <link rel="icon" href="/favicon.png" />
+    </>
+  );
+}
